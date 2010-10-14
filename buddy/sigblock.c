d0567d5fbc23c23031d9897aa290944c27d54714
Greg Olszewski <gno@gropeep.org>
2010-10-13 20:43:14 -0700

    sigblock.{c,h} should be replaced with public domain version in daemontools.

--- buddy/sigblock.c
+++ buddy/sigblock.c
@@ -1,22 +1,23 @@
+#include <string.h>
 #include <signal.h>
 
-void block_signal(int signal) { 
+void upk_block_signal(int signal) { 
   sigset_t set;
   sigemptyset(&set);
   sigaddset(&set,signal);
-  sigprocmask(SIG_BLOCK, set,NULL);
+  sigprocmask(SIG_BLOCK, &set, NULL);
 }
 
-void unblock_signal(int signal) { 
+void upk_unblock_signal(int signal) { 
   sigset_t set;
   sigemptyset(&set);
   sigaddset(&set,signal);
-  sigprocmask(SIG_UNBLOCK, set, NULL);
+  sigprocmask(SIG_UNBLOCK, &set, NULL);
 }
 
-void catch_signal(int signal, void (*f)(int)) { 
-  struct sigaction sa = { NULL };
-  sigemptyset(sa.sa_mask);
+void upk_catch_signal(int signal, void (*f)(int)) { 
+  struct sigaction sa = {};
+  sigemptyset(&(sa.sa_mask));
 
   sa.sa_handler = f;
   sigaction(signal,&sa,NULL);
