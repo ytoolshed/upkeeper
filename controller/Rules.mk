a0fa6b2c20de61fcb8416f1bb4e5a6769074091a
Mike Schilli <mschilli@yahoo-inc.com>
2010-10-20 15:07:23 -0700

    fixed controller makefile

--- controller/Rules.mk
+++ controller/Rules.mk
@@ -5,13 +5,10 @@ $(DEPS)	:= $(OBJS_$(d):.o=.d)
 $(BIN)	:= $(d)/buddy-controller
 
 CF_$(d) += -I$(d) -Istore
-LL_$(d) := store/store.a $(d)/buddy.a deps/sqlite/sqlite3.a common/common.a
+LL_$(d) := store/store.a buddy/buddy.a deps/sqlite/sqlite3.a common/common.a
 
-CLEAN	+= $(OBJS_$(d)) $(DEPS_$(d))
-CHECK	+= $(d)/buddytest.tap
+CHECK	+= $(d)/controller.tap
 
-$(d)/buddy.a:   $($(OBJS))
-	$(ARCH)
 $(d)/buddy-controller.tap: $(d)/buddy-controller
 $(d)/buddy-controller: $(d)/main.c $(LL_$(d))
 	$(COMPLINK)
