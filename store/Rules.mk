601f7811b9d3e65a1fa8d284da43d2f4288fd0f4
Mike Schilli <mschilli@yahoo-inc.com>
2010-10-20 17:16:46 -0700

    added store-test exe

--- store/Rules.mk
+++ store/Rules.mk
@@ -4,8 +4,10 @@ include begin.mk
 # objects, targets
 $(OBJS) 	:= $(d)/util.o $(d)/upk_db.o $(d)/test.o
 $(DEPS)		:= $(OBJS_$(d):.o=.d)
-$(BIN)		:= $(d)/upk
+$(BIN)		:= $(d)/store-test
 $(LIB)		:= $(d)/store.a
+
+LL_$(d) := store/store.a deps/sqlite/sqlite3.a
 
 $(d)/upk: $(d)/main.c $(d)/store.a deps/sqlite/sqlite3.a
 	$(COMPLINK)
@@ -14,6 +16,9 @@ $(d)/store.a: $($(OBJS))
 	$(ARCH)
 
 CLEAN		:= $(CLEAN) store.sqlite
+
+$(d)/store-test: $(d)/main.c $(LL_$(d))
+	$(COMPLINK)
 
 # Standard things
 
