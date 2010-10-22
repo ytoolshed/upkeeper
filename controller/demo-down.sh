54b1491fc6f9ab6cdb5c19bc43dc40dcfc6982d9
Mike Schilli <m@perlmeister.com>
2010-10-22 12:05:02 -0700

    demo sleep out

--- controller/demo-down.sh
+++ controller/demo-down.sh
@@ -4,7 +4,7 @@ do
   ./upk --define  package-$i service-$i ""
   ./upk --set-pid package-$i service-$i 0
   ./upk --set-actual-state package-$i service-$i stop
-  sleep 1
+  # sleep 1
 done
 
 ./upk --all-down
--- controller/demo-up.sh
+++ controller/demo-up.sh
@@ -4,5 +4,5 @@ do
   ./upk --define  package-$i service-$i sleep 360$i
   ./upk --set-pid package-$i service-$i 12$i
   ./upk --set-actual-state package-$i service-$i start
-  sleep 1
+  # sleep 1
 done
