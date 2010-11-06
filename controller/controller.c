f5def14df9561e0ca507ffbba87ba0acf58f9494
mschilli <github@perlmeister.com>
2010-11-05 18:02:30 -0700

    status fixer now gets 'buddy: fatal: failed to exec file does not exist' but is roughly functionable.

--- controller/controller.c
+++ controller/controller.c
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include "controller.h"
 
+extern int DEBUG;
+
 /* 
  * Bring a service inline with the desired status
  */
@@ -14,16 +16,35 @@ void upk_controller_status_fixer_callback(
     char    *status_desired,
     char    *status_actual
 ) {
+    const char *cmdline;
+
     if( strcmp( status_desired, status_actual ) == 0 ) {
-	return;
+	return; /* We're good */
+    }
+
+    cmdline = upk_db_service_cmdline( srvc, NULL );
+
+    if( cmdline == NULL ) {
+        if( DEBUG ) {
+	    printf("** ERROR no cmdline for %s/%s\n", 
+                   srvc->package, srvc->service );
+        }
+        return;
     }
 
     if( strcmp( status_desired, upk_states[ UPK_STATUS_VALUE_START ] ) == 0 ) {
 	/* service needs to be started */
-	printf("** STARTING %s/%s\n", srvc->package, srvc->service );
+        if( DEBUG ) {
+	    printf("** STARTING %s/%s/%s\n", srvc->package, srvc->service,
+                                             cmdline );
+        }
+        upk_buddy_start( srvc, cmdline, NULL );
     } else {
-	/* service needs to be stopped */
-	printf("** STOPPING %s/%s\n", srvc->package, srvc->service );
+        if( DEBUG ) {
+	    printf("** STOPPING %s/%s/%s\n", srvc->package, srvc->service,
+                                             cmdline );
+        }
+        upk_buddy_stop( srvc );
     }
 
     return;
