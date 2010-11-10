21314a09f954d0fc3cf315d457e8d0cbd3323bd4
Mike Schilli <mschilli@yahoo-inc.com>
2010-11-10 13:56:22 -0800

    demo setup changes

--- demo/demo-setup.sh
+++ demo/demo-setup.sh
@@ -5,3 +5,11 @@ rm -f store.sqlite
 
 cd ../controller
 ./upk --init
+
+for i in 0 1 2 3 4 5
+do
+  ./upk --define  package-$i service-$i ../demo/sleep10.sh
+  # ./upk --set-desired-state package-$i service-$i start
+  # ./upk --set-desired-state package-$i service-$i stop
+  # sleep 1
+done
