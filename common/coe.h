c07b4240441c9b6e3c93b3948883f27789145c87
Greg Olszewski <gno@keepsleep.greatamerica.corp.yahoo.com>
2010-11-10 12:30:41 -0800

    o reimplement coe using fcntl(2) as guide.

--- common/coe.h
+++ common/coe.h
@@ -1,8 +1,16 @@
-/* Public domain. */
+/***
+ * Copyright (C), 2010 Yahoo! Inc. All rights reserved.
+ *
+ * Written by Greg Olszewski around Wed Nov 10 12:28:05 2010
+ *
+ * COE: "Close on Exec". concept from daemontools.
+ */
 
-#ifndef COE_H
-#define COE_H
+#ifndef __COE_H__
+#define __COE_H__
 
-extern int coe(int);
+
+void coe(int fd);
+
 
 #endif
