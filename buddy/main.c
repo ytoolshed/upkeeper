a81d2b5c37154a4a05ec7dabac64dee7d0884f0d
Greg Olszewski <gno@keepsleep.greatamerica.corp.yahoo.com>
2010-11-10 17:09:57 -0800

    o performance test -- caution, probably breaks 'make check'

--- buddy/main.c
+++ buddy/main.c
@@ -13,14 +13,14 @@ int main(
     int   argc, 
     char *argv[] 
 ) {
-    char    *file = "../store/store.sqlite";
+    char    *file = "./store.sqlite";
     int      rc;
     int      i;
     int      pid;
     char    *event;
     char    *status;
     const char    *cp;
-    const char    *command = "./do-sleep";
+    const char    *command = "../do-sleep";
     struct upk_srvc s = {NULL, "package-1", "service-1" };
 
     printf("1..5\n");
@@ -28,7 +28,7 @@ int main(
     rc = upk_db_init( file, &s.pdb );
 
     if(rc < 0) {
-	printf("db_init failed. Exiting.\n");
+	printf("db_init failed. nnExiting.\n");
 	exit(-1);
     }
 
@@ -36,20 +36,34 @@ int main(
     upk_test_eq( cp, "stop" );
     cp = upk_db_service_actual_status(  &s, UPK_STATUS_VALUE_START );
     upk_test_eq( cp, "start" );
-
+    upk_db_service_cmdline(&s, command);
+    cp = upk_db_service_desired_status(  &s, UPK_STATUS_VALUE_START );
+    upk_test_eq( cp, "start" );
     pid = upk_buddy_start( &s, command, NULL);
     upk_test_isnt(pid,-1, "upk_buddy_start returned positive id");
-
     if( pid < 0 ) {
 	printf( "upk_buddy_start failed (%d)\n", pid );
 	exit( -1 );
     }
+
+    s.service=strdup("service                 ");
+    for (i = 0 ; i < 1024 ; i++) {
+      sprintf(s.service,"svc-%d",i);
+      pid = upk_buddy_start_async( &s, command, NULL);
+    }
+
     for (;;) {
       cp = upk_db_service_actual_status( &s, 0 );
       if (!strcmp(cp,"start")) { break; }
+      sleep(3);
     }
     upk_test_is (upk_buddy_stop( &s ),0, "stopping the buddy returned 0");
     upk_test_eq( cp, "stop");
+    s.service="talker";
+    upk_db_service_cmdline(&s, "../talker");
+    pid = upk_buddy_start( &s, "../talker", NULL);
+    upk_buddy_stop( &s );
+    wait();
     
     sqlite3_close( s.pdb );
 }
