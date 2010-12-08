db669a09a23074512a19b753713617352e20ab7b
Greg Olszewski <gno@yahoo-inc.com>
2010-12-07 18:00:05 -0800

    o buddytest.t generates an empty database as part of the test.

--- buddy/Rules.mk
+++ buddy/Rules.mk
@@ -9,11 +9,12 @@ CF_$(d) += -I$(d) -Istore
 LL_$(d) := store/store.a $(d)/buddy.a deps/sqlite/sqlite3.a common/common.a
 
 CHECK	+= $(d)/buddytest.tap
+CLEAN   += $(d)/bt/store.sqlite $(d)/bt/buddy
 
 $(d)/buddy.a:   $($(OBJS))
 	$(ARCH)
-$(d)/buddytest.tap: $(d)/buddytest
-$(d)/buddytest: $(d)/main.c $(LL_$(d))
+$(d)/buddytest.tap: $(d)/buddytest 
+$(d)/buddytest: $(d)/main.c $(LL_$(d)) $(d)/buddy
 	$(COMPLINK) $(LL_buddy)
 
 $(d)/buddy: $(d)/buddy.c $(LL_$(d))
--- buddy/buddytest.t
+++ buddy/buddytest.t
@@ -2,4 +2,4 @@
 cd `dirname $0`
 (mkdir -p bt || echo ok)
 (ln -f buddy bt/)
-(cd bt && ../buddytest)
+cd bt && rm -f store.sqlite && sqlite3 store.sqlite < ../../store/schema.sql && exec ../buddytest
