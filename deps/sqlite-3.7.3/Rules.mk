06bd5ff5c326ffd6d674832de318272d1a928464
Greg Olszewski <gno@yahoo-inc.com>
2010-12-15 11:19:57 -0800

    o drop -ldl

--- deps/sqlite-3.7.3/Rules.mk
+++ deps/sqlite-3.7.3/Rules.mk
@@ -7,7 +7,7 @@ $(OBJS) 	:= $(d)/sqlite3.o
 $(DEPS)		:= $(OBJS_$(d):%.o=%.d)
 $(LIB)		:= $(d)/sqlite3.a
 
-$(d)/sqlite3.o: CF_TGT		:= -DSQLITE_THREADSAFE=0 
+$(d)/sqlite3.o: CF_TGT		:= -DSQLITE_THREADSAFE=0 -DSQLITE_OMIT_LOAD_EXTENSION=1
 $(d)/sqlite3.a:   $($(OBJS))
 		$(ARCH)
 
