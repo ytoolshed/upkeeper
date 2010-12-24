54d3bf3be37f625672ed13228fa91cdf53574e72
mschilli <github@perlmeister.com>
2010-12-23 17:07:31 -0800

    setters done

--- uptop/render.c
+++ uptop/render.c
@@ -19,6 +19,16 @@ struct table_row {
 
 struct table_item *table_item_new( void );
 
+void table_item_format_set(
+    struct table_item *item,
+    char *format
+);
+
+void table_item_format_set(
+    struct table_item *item,
+    char *format
+);
+
 void table_item_free(
     struct table_item *item
 );
@@ -37,6 +47,51 @@ struct table_item *table_item_new( void ) {
     item->active  = 1;
     item->content = NULL;
     item->format  = NULL;
+}
+
+/*
+ * Dump the content of a table item
+ */
+void table_item_dump(
+    struct table_item *item
+) {
+    if( item == NULL ) {
+        printf( "table_item: NULL\n" );
+        return;
+    }
+
+    printf( "table_item: content='%s' format='%s'\n",
+            item->content == NULL ? "NULL" : item->content,
+            item->format  == NULL ? "NULL" : item->format
+          );
+
+    return;
+}
+
+/*
+ * Set the content of a table item
+ */
+void table_item_content_set(
+    struct table_item *item,
+    char *content
+) {
+    char *dup = strdup( content );
+    assert( dup != NULL );
+
+    item->content = dup;
+}
+
+/*
+ * Set the format of a table item
+ */
+void table_item_format_set(
+    struct table_item *item,
+    char *format
+) {
+    char *dup = strdup( format );
+    assert( dup != NULL );
+
+    item->format = dup;
 }
 
 /*
@@ -63,5 +118,10 @@ main() {
     struct table_item *item;
 
     item = table_item_new();
+
+    table_item_format_set( item, "%05d" );
+    table_item_content_set( item, "123" );
+    table_item_dump( item );
+
     table_item_free( item );
 }
