37419bc767269b22a6355f326da6ebd587e84370
greg olszewski <greg@devicethis.(none)>
2011-02-02 14:32:16 -0800

    o build an sqlite3 binary and use it during 'make check'

--- buddy/buddytest.t
+++ buddy/buddytest.t
@@ -1,6 +1,6 @@
-#!/bin/sh 
+#!/bin/sh -x
 DIR=`dirname $(which $0)`
 DIR=$( (cd $DIR ; pwd -P) )
-PATH=$PATH:$DIR
+PATH=$PATH:$DIR:$DIR/../deps/sqlite
 (cd $DIR && mkdir -p bt || echo ok)
 cd $DIR/bt && rm -f store.sqlite && sqlite3 store.sqlite < ../../store/schema.sql && exec ../buddytest
--- deps/sqlite-3.7.3/Rules.mk
+++ deps/sqlite-3.7.3/Rules.mk
@@ -6,9 +6,15 @@ include begin.mk
 $(OBJS) 	:= $(d)/sqlite3.o
 $(DEPS)		:= $(OBJS_$(d):%.o=%.d)
 $(LIB)		:= $(d)/sqlite3.a
+$(BIN)          := $(d)/sqlite3
 
-$(d)/sqlite3.o: CF_TGT		:= -DSQLITE_THREADSAFE=0 -DSQLITE_OMIT_LOAD_EXTENSION=1
+$(d)/sqlite3 $(d)/sqlite3.o: CF_TGT := -DSQLITE_THREADSAFE=0
+$(d)/sqlite3 $(d)/sqlite3.o: CF_TGT += -DSQLITE_OMIT_LOAD_EXTENSION=1
 $(d)/sqlite3.a:   $($(OBJS))
-		$(ARCH)
+	$(ARCH)
+
+$(d)/sqlite3: $(d)/shell.c $(d)/sqlite3.a
+	$(COMPLINK) deps/sqlite/sqlite3.a
+
 
 include end.mk
