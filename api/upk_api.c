3189d6f9c82c54b78cf2e22f3eb647ec862b89e3
mschilli <github@perlmeister.com>
2011-02-17 07:58:05 -0800

    api skeleton added

--- api/upk_api.c
+++ api/upk_api.c
@@ -1,6 +1,76 @@
+/* 
+ * upkeeper's external API
+ */
+#include <libgen.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+#include <fcntl.h>
+#include <time.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <errno.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 
-int func(
+#include "store/upk_db.h"
+#include "api/upk_api.h"
+
+/* 
+ * Initialize
+ */
+int 
+upk_api_init(
+    struct upk_api *pupk_api
 ) {
-    return( 0 );
+    int rc = 0;
+
+    rc = upk_db_init( &pupk_api->upk_db );
+
+    return( rc );
+}
+
+/* 
+ * 
+ */
+int
+upk_api_service_desired_status_set(
+    struct upk_api *pupk_api,
+    char           *package,
+    char           *service,
+    upk_status_t    upk_status
+) {
+    int rc = 0;
+
+    return( rc );
+}
+
+/* 
+ * 
+ */
+int 
+upk_api_service_desired_status_get(
+    struct upk_api *pupk_api,
+    char           *package,
+    char           *service
+) {
+    int rc = 0;
+
+    return( rc );
+}
+
+int 
+upk_api_service_actual_status_get(
+    struct upk_api *pupk_api,
+    char           *package,
+    char           *service
+) {
+    int rc = 0;
+
+    return( rc );
 }
--- api/upk_api.h
+++ api/upk_api.h
@@ -3,4 +3,35 @@
 
 #include "store/upk_db.h"
 
+typedef struct upk_api {
+    struct upk_db upk_db;
+} upk_api_t;
+
+int 
+upk_api_init(
+    struct upk_api *pupk_api
+);
+
+int 
+upk_api_service_desired_status_get(
+    struct upk_api *pupk_api,
+    char           *package,
+    char           *service
+);
+
+int 
+upk_api_service_actual_status_set(
+    struct upk_api *pupk_api,
+    char           *package,
+    char           *service,
+    upk_status_t    upk_status
+);
+
+int 
+upk_api_service_actual_status_get(
+    struct upk_api *pupk_api,
+    char           *package,
+    char           *service
+);
+
 #endif
