f9eeae3759fc9b685d101078a74a9eeef80628b2
Jamie Beverly <cthulhu@yahoo-inc.com>
2011-05-19 02:20:03 +0400

    macro-ized helpers for sanity

--- libupkeeper/upkeeper/upk_v0_protocol.c
+++ libupkeeper/upkeeper/upk_v0_protocol.c
@@ -1,35 +1,78 @@
 #include "v0_protocol.h"
 #define PROTOCOL_VERSION 0
-#define CALC_PTR_LEN(M) (payload->M - sizeof(size_t))      /* the resulting payload size will have a size_t subtracted
-                                                            * from it (the pointer itself) and instead have the length
-                                                            * of data that was pointed to in its place; its easier to
-                                                            * calculate that here */
 
-#define CALC_ENUM_LEN(M) (sizeof(uint32_t) - sizeof(payload->M))
-#define CALC_BOOL_LEN(M) (sizeof(uint8_t) - sizeof(payload-M))
+#define CALC_ENUM_LEN(THING) 0
+#define CALC_PTR_LEN(THING) 0
+#define CALC_BOOL_LEN(THING) 0
 
 #define COPY_STRING_MEMB(MEMB, LEN) payload->LEN = strnlen(MEMB, UPK_MAX_STRING_LEN); \
                                payload->MEMB = calloc(1, payload->LEN + 1); \
                                strncpy(payload->MEMB, MEMB, payload->LEN + 1)
 
 
+
+#define UPK_INIT_HELPER(TYPE, MSGTYPE) \
+    TYPE *UPK_DATA = NULL; \
+    uint32_t UPK_DATA_LEN = 0; \
+    size_t UPK_STRING_LENGTH = 0; \
+    UPK_DATA = calloc(1, sizeof(*UPK_DATA)); \
+    UPK_DATA->msgtype = MSGTYPE
+
+#define UPK_HELPER_UINT32_EXPLICIT(DEST,SRC) \
+    DEST = SRC; \
+    UPK_DATA_LEN += sizeof(uint32_t)
+
+#define UPK_HELPER_UINT32(MEMB) \
+    UPK_HELPER_UINT32_EXPLICIT(UPK_DATA->MEMB, MEMB);
+
+#define UPK_HELPER_UINT16_EXPLICIT(DEST,SRC) \
+    DEST = SRC; \
+    UPK_DATA_LEN += sizeof(uint16_t)
+
+#define UPK_HELPER_UINT16(MEMB) \
+    UPK_HELPER_UINT16_EXPLICIT(UPK_DATA->MEMB, MEMB);
+
+#define UPK_HELPER_UINT8_EXPLICIT(DEST,SRC) \
+    DEST = SRC; \
+    UPK_DATA_LEN += sizeof(uint8_t)
+
+#define UPK_HELPER_UINT8(MEMB) \
+    UPK_HELPER_UINT8_EXPLICIT(UPK_DATA->MEMB, MEMB);
+
+#define UPK_HELPER_ENUM(MEMB) \
+    UPK_HELPER_UINT32(MEMB)
+
+#define UPK_HELPER_BOOL(MEMB) \
+    UPK_HELPER_UINT8(MEMB)
+
+#define UPK_HELPER_BUF(DEST, SRC, LEN) \
+    DEST = calloc(1, LEN); \
+    memcpy(DEST,SRC,LEN); \
+    UPK_DATA_LEN += LEN
+
+#define UPK_HELPER_ANSISTRING(DEST, SRC) \
+    UPK_STRING_LENGTH = strnlen(SRC, UPK_MAX_STRING_LEN); \
+    UPK_HELPER_BUF(DEST, SRC, UPK_STRING_LENGTH + 1); \
+    UPK_DATA_LEN--                                         /* drop the null from the byte count */
+
+#define UPK_HELPER_STRING(MEMB, LEN_MEMB) \
+    UPK_HELPER_ANSISTRING(UPK_DATA->MEMB, MEMB); \
+    UPK_HELPER_UINT32_EXPLICIT(UPK_DATA->LEN_MEMB, UPK_STRING_LENGTH)
+
+
+#define UPK_END_HELPER(PKT_TYPE) return upk_create_pkt(UPK_DATA, UPK_DATA_LEN, PKT_TYPE, PROTOCOL_VERSION)
+
 /* *******************************************************************************************************************
  * ****************************************************************************************************************** */
 upk_packet_t           *
 v0_create_req_seq_start(upk_req_msgtype_t msg_seq_type, uint32_t msg_seq_count)
 {
-    v0_req_seq_start_t     *payload = NULL;
-    uint32_t                payload_len = 0;
+    UPK_INIT_HELPER(v0_req_seq_start_t, REQ_SEQ_START);
 
-    payload = calloc(1, sizeof(*payload));
+    UPK_HELPER_ENUM(msg_seq_type);
+    UPK_HELPER_UINT32(msg_seq_count);
 
-    payload->msgtype = REQ_SEQ_START;
-    payload->msg_seq_type = msg_seq_type;
-    payload->msg_seq_count = msg_seq_count;
-
-    payload_len =
-        sizeof(*payload) + CALC_ENUM_LEN(msgtype) + CALC_ENUM_LEN(msg_seq_type) + CALC_ENUM_LEN(msg_seq_count);
-    return upk_create_pkt(payload, payload_len, PKT_REQUEST, PROTOCOL_VERSION);
+    UPK_END_HELPER(PKT_REQUEST);
 }
 
 
@@ -38,16 +81,11 @@ v0_create_req_seq_start(upk_req_msgtype_t msg_seq_type, uint32_t msg_seq_count)
 upk_packet_t           *
 v0_create_req_seq_end(bool commit)
 {
-    v0_req_seq_end_t       *payload = NULL;
-    uint32_t                payload_len = 0;
+    UPK_INIT_HELPER(v0_req_seq_end_t, REQ_SEQ_END);
 
-    payload = calloc(1, sizeof(*payload));
+    UPK_HELPER_BOOL(commit);
 
-    payload->msgtype = REQ_SEQ_END;
-    payload->commit = commit;
-
-    payload_len = sizeof(*payload) + CALC_ENUM_LEN(msgtype) + CALC_BOOL_LEN(commit);
-    return upk_create_pkt(payload, payload_len, PKT_REQUEST, PROTOCOL_VERSION);
+    UPK_END_HELPER(PKT_REQUEST);
 }
 
 
@@ -56,17 +94,12 @@ v0_create_req_seq_end(bool commit)
 upk_packet_t           *
 v0_create_action_req(char *svc_id, char *action)
 {
-    v0_action_req_t        *payload = NULL;
-    uint32_t                payload_len = 0;
+    UPK_INIT_HELPER(v0_action_req_t, REQ_ACTION);
 
-    payload = calloc(1, sizeof(*payload));
+    UPK_HELPER_STRING(svc_id, svc_id_len);
+    UPK_HELPER_STRING(action, action_len);
 
-    payload->msgtype = REQ_ACTION;
-    COPY_STRING_MEMB(svc_id, svc_id_len);
-    COPY_STRING_MEMB(action, action_len);
-
-    payload_len = sizeof(*payload) + CALC_ENUM_LEN(msgtype) + CALC_PTR_LEN(svc_id_len) + CALC_PTR_LEN(action_len);
-    return upk_create_pkt(payload, payload_len, PKT_REQUEST, PROTOCOL_VERSION);
+    UPK_END_HELPER(PKT_REQUEST);
 }
 
 /* *******************************************************************************************************************
@@ -74,21 +107,14 @@ v0_create_action_req(char *svc_id, char *action)
 upk_packet_t           *
 v0_create_signal_req(char *svc_id, uint8_t signal, bool signal_sid, bool signal_pgrp)
 {
-    v0_signal_req_t        *payload = NULL;
-    uint32_t                payload_len = 0;
+    UPK_INIT_HELPER(v0_signal_req_t, REQ_SIGNAL);
 
-    payload = calloc(1, sizeof(*payload));
+    UPK_HELPER_ENUM(signal);
+    UPK_HELPER_BOOL(signal_sid);
+    UPK_HELPER_BOOL(signal_pgrp);
+    UPK_HELPER_STRING(svc_id, svc_id_len);
 
-    payload->msgtype = REQ_SIGNAL;
-    payload->signal = signal;
-    payload->signal_sid = signal_sid;
-    payload->signal_pgrp = signal_pgrp;
-    COPY_STRING_MEMB(svc_id, svc_id_len);
-
-    payload_len =
-        sizeof(*payload) + CALC_ENUM_LEN(msgtype) + CALC_BOOL_LEN(signal_sid) + CALC_BOOL_LEN(signal_pgrp) +
-        CALC_PTR_LEN(svc_id_len);
-    return upk_create_pkt(payload, payload_len, PKT_REQUEST, PROTOCOL_VERSION);
+    UPK_END_HELPER(PKT_REQUEST);
 }
 
 
@@ -97,15 +123,8 @@ v0_create_signal_req(char *svc_id, uint8_t signal, bool signal_sid, bool signal_
 upk_packet_t           *
 v0_create_list_req(void)
 {
-    v0_list_req_t          *payload = NULL;
-    uint32_t                payload_len = 0;
-
-    payload = calloc(1, sizeof(*payload));
-
-    payload->msgtype = REQ_LIST;
-
-    payload_len = sizeof(*payload) + CALC_ENUM_LEN(msgtype);
-    return upk_create_pkt(payload, payload_len, PKT_REQUEST, PROTOCOL_VERSION);
+    UPK_INIT_HELPER(v0_list_req_t, REQ_LIST);
+    UPK_END_HELPER(PKT_REQUEST);
 }
 
 
@@ -114,16 +133,11 @@ v0_create_list_req(void)
 upk_packet_t           *
 v0_create_status_req(char *svc_id)
 {
-    v0_status_req_t        *payload = NULL;
-    uint32_t                payload_len = 0;
+    UPK_INIT_HELPER(v0_status_req_t, REQ_STATUS);
 
-    payload = calloc(1, sizeof(*payload));
+    UPK_HELPER_STRING(svc_id, svc_id_len);
 
-    payload->msgtype = REQ_STATUS;
-    COPY_STRING_MEMB(svc_id, svc_id_len);
-
-    payload_len = sizeof(*payload) + CALC_ENUM_LEN(msgtype) + CALC_PTR_LEN(svc_id_len);
-    return upk_create_pkt(payload, payload_len, PKT_REQUEST, PROTOCOL_VERSION);
+    UPK_END_HELPER(PKT_REQUEST);
 }
 
 
@@ -132,16 +146,12 @@ v0_create_status_req(char *svc_id)
 upk_packet_t           *
 v0_create_subscr_req(char *svc_id, bool all_svcs)
 {
-    v0_subscribe_req_t     *payload = NULL;
-    uint32_t                payload_len = 0;
+    UPK_INIT_HELPER(v0_subscribe_req_t, REQ_SUBSCRIBE);
 
-    payload = calloc(1, sizeof(*payload));
+    UPK_HELPER_BOOL(all_svcs);
+    UPK_HELPER_STRING(svc_id, svc_id_len);
 
-    payload->msgtype = REQ_SUBSCRIBE;
-    COPY_STRING_MEMB(svc_id, svc_id_len);
-
-    payload_len = sizeof(*payload) + CALC_ENUM_LEN(msgtype) + CALC_PTR_LEN(svc_id_len);
-    return upk_create_pkt(payload, payload_len, PKT_REQUEST, PROTOCOL_VERSION);
+    UPK_END_HELPER(PKT_REQUEST);
 }
 
 
@@ -151,17 +161,12 @@ v0_create_subscr_req(char *svc_id, bool all_svcs)
 upk_packet_t           *
 v0_create_unsubs_req(char *svc_id, bool all_svcs)
 {
-    v0_unsubscribe_req_t   *payload = NULL;
-    uint32_t                payload_len = 0;
+    UPK_INIT_HELPER(v0_unsubscribe_req_t, REQ_UNSUBSCRIBE);
 
-    payload = calloc(1, sizeof(*payload));
+    UPK_HELPER_BOOL(all_svcs);
+    UPK_HELPER_STRING(svc_id, svc_id_len);
 
-    payload->msgtype = REQ_UNSUBSCRIBE;
-    payload->all_svcs = all_svcs;
-    COPY_STRING_MEMB(svc_id, svc_id_len);
-
-    payload_len = sizeof(*payload) + CALC_ENUM_LEN(msgtype) + CALC_BOOL_LEN(all_svcs) + CALC_PTR_LEN(svc_id_len);
-    return upk_create_pkt(payload, payload_len, PKT_REQUEST, PROTOCOL_VERSION);
+    UPK_END_HELPER(PKT_REQUEST);
 }
 
 /* *******************************************************************************************************************
@@ -169,15 +174,8 @@ v0_create_unsubs_req(char *svc_id, bool all_svcs)
 upk_packet_t           *
 v0_create_disconnect_req(void)
 {
-    v0_disconnect_req_t    *payload = NULL;
-    uint32_t                payload_len = 0;
-
-    payload = calloc(1, sizeof(*payload));
-
-    payload->msgtype = REQ_DISCONNECT;
-
-    payload_len = sizeof(*payload) + CALC_ENUM_LEN(msgtype);
-    return upk_create_pkt(payload, payload_len, PKT_REQUEST, PROTOCOL_VERSION);
+    UPK_INIT_HELPER(v0_disconnect_req_t, REQ_DISCONNECT);
+    UPK_END_HELPER(PKT_REQUEST);
 }
 
 /* *******************************************************************************************************************
@@ -185,18 +183,12 @@ v0_create_disconnect_req(void)
 upk_packet_t           *
 v0_create_repl_seq_start(upk_repl_msgtype_t msg_seq_type, uint32_t msg_seq_count)
 {
-    v0_repl_seq_start_t    *payload = NULL;
-    uint32_t                payload_len = 0;
+    UPK_INIT_HELPER(v0_repl_seq_start_t, REPL_SEQ_START);
 
-    payload = calloc(1, sizeof(*payload));
+    UPK_HELPER_ENUM(msg_seq_type);
+    UPK_HELPER_UINT32(msg_seq_count);
 
-    payload->msgtype = REPL_SEQ_START;
-    payload->msg_seq_type = msg_seq_type;
-    payload->msg_seq_count = msg_seq_count;
-
-    payload_len =
-        sizeof(*payload) + CALC_ENUM_LEN(msgtype) + CALC_ENUM_LEN(msg_seq_type) + CALC_ENUM_LEN(msg_seq_count);
-    return upk_create_pkt(payload, payload_len, PKT_REPLY, PROTOCOL_VERSION);
+    UPK_END_HELPER(PKT_REPLY);
 }
 
 /* *******************************************************************************************************************
@@ -204,16 +196,10 @@ v0_create_repl_seq_start(upk_repl_msgtype_t msg_seq_type, uint32_t msg_seq_count
 upk_packet_t           *
 v0_create_repl_seq_end(bool commit)
 {
-    v0_repl_seq_end_t      *payload = NULL;
-    uint32_t                payload_len = 0;
+    UPK_INIT_HELPER(v0_repl_seq_end_t, REPL_SEQ_END);
 
-    payload = calloc(1, sizeof(*payload));
-
-    payload->msgtype = REPL_SEQ_END;
-    payload->commit = commit;
-
-    payload_len = sizeof(*payload) + CALC_ENUM_LEN(msgtype) + CALC_BOOL_LEN(commit);
-    return upk_create_pkt(payload, payload_len, PKT_REPLY, PROTOCOL_VERSION);
+    UPK_HELPER_BOOL(commit);
+    UPK_END_HELPER(PKT_REPLY);
 }
 
 /* *******************************************************************************************************************
@@ -221,17 +207,12 @@ v0_create_repl_seq_end(bool commit)
 upk_packet_t           *
 v0_create_result_repl(char *msg, bool successful)
 {
-    v0_result_repl_t       *payload = NULL;
-    uint32_t                payload_len = 0;
+    UPK_INIT_HELPER(v0_result_repl_t, REPL_RESULT);
 
-    payload = calloc(1, sizeof(*payload));
+    UPK_HELPER_BOOL(successful);
+    UPK_HELPER_STRING(msg, msg_len);
 
-    payload->msgtype = REPL_RESULT;
-    payload->successful = successful;
-    COPY_STRING_MEMB(msg, msg_len);
-
-    payload_len = sizeof(*payload) + CALC_ENUM_LEN(msgtype) + CALC_BOOL_LEN(successful) + CALC_PTR_LEN(msg_len);
-    return upk_create_pkt(payload, payload_len, PKT_REPLY, PROTOCOL_VERSION);
+    UPK_END_HELPER(PKT_REPLY);
 }
 
 
@@ -240,16 +221,11 @@ v0_create_result_repl(char *msg, bool successful)
 upk_packet_t           *
 v0_create_listing_repl(char *svc_id)
 {
-    v0_listing_repl_t      *payload = NULL;
-    uint32_t                payload_len = 0;
+    UPK_INIT_HELPER(v0_listing_repl_t, REPL_LISTING);
 
-    payload = calloc(1, sizeof(*payload));
+    UPK_HELPER_STRING(svc_id, svc_id_len);
 
-    payload->msgtype = REPL_LISTING;
-    COPY_STRING_MEMB(svc_id, svc_id_len);
-
-    payload_len = sizeof(*payload) + CALC_ENUM_LEN(msgtype) + CALC_PTR_LEN(svc_id_len);
-    return upk_create_pkt(payload, payload_len, PKT_REPLY, PROTOCOL_VERSION);
+    UPK_END_HELPER(PKT_REPLY);
 }
 
 /* *******************************************************************************************************************
@@ -259,17 +235,12 @@ get_svcinfo_len(v0_svcinfo_t svcinfo)
 {
     uint32_t                length = 0;
 
-    length += sizeof(svcinfo.last_action_time);
-    length += sizeof(svcinfo.last_action_status);
-    length += sizeof(svcinfo.last_action_name_len);
-    length += svcinfo.last_action_name_len;
-    length += sizeof(svcinfo.last_signal_time);
-    length += sizeof(svcinfo.last_signal_status);
-    length += sizeof(uint32_t);                            /* upk_signal_name_t last_signal_name; */
-    length += sizeof(svcinfo.buddy_pid);
-    length += sizeof(svcinfo.proc_pid);
-    length += sizeof(uint32_t);                            /* upk_state_t current_state; */
-    length += sizeof(uint32_t);                            /* upk_state_t prior_state); */
+    length = sizeof(svcinfo);                              /* any non-portable types must be specially handled here */
+
+    length += sizeof(uint32_t) - sizeof(upk_signal_name_t);
+    length += sizeof(uint32_t) - sizeof(upk_state_t);
+    length += sizeof(uint32_t) - sizeof(upk_state_t);
+    length += svcinfo.last_action_name_len - sizeof(size_t);
 
     return length;
 }
@@ -279,19 +250,18 @@ get_svcinfo_len(v0_svcinfo_t svcinfo)
 upk_packet_t           *
 v0_create_svcinfo_repl(char *svc_id, v0_svcinfo_t * svcinfo)
 {
-    v0_svcinfo_repl_t      *payload = NULL;
-    uint32_t                payload_len = 0;
+    UPK_INIT_HELPER(v0_svcinfo_repl_t, REPL_SVCINFO);
 
-    payload = calloc(1, sizeof(*payload));
+    svcinfo->last_action_name_len = strnlen(svcinfo->last_action_name, UPK_MAX_STRING_LEN);
 
-    payload->msgtype = REPL_SVCINFO;
-    payload->svcinfo_len = get_svcinfo_len(payload->svcinfo);
-    memcpy(&payload->svcinfo, svcinfo, sizeof(payload->svcinfo));
+    UPK_STRING_LENGTH = get_svcinfo_len(*svcinfo);
+    UPK_HELPER_UINT32_EXPLICIT(UPK_DATA->svcinfo_len, UPK_STRING_LENGTH);
+    memcpy(&UPK_DATA->svcinfo, svcinfo, sizeof(*svcinfo));
+    UPK_DATA_LEN += UPK_STRING_LENGTH;
 
-    COPY_STRING_MEMB(svc_id, svc_id_len);
+    UPK_HELPER_STRING(svc_id, svc_id_len);
 
-    payload_len = sizeof(*payload) + CALC_ENUM_LEN(msgtype) + CALC_PTR_LEN(svc_id_len);
-    return upk_create_pkt(payload, payload_len, PKT_REPLY, PROTOCOL_VERSION);
+    UPK_END_HELPER(PKT_REPLY);
 }
 
 /* *******************************************************************************************************************
@@ -299,15 +269,8 @@ v0_create_svcinfo_repl(char *svc_id, v0_svcinfo_t * svcinfo)
 upk_packet_t           *
 v0_create_ack_repl(void)
 {
-    v0_ack_repl_t          *payload = NULL;
-    uint32_t                payload_len = 0;
-
-    payload = calloc(1, sizeof(*payload));
-
-    payload->msgtype = REPL_ACK;
-
-    payload_len = sizeof(*payload) + CALC_ENUM_LEN(msgtype);
-    return upk_create_pkt(payload, payload_len, PKT_REPLY, PROTOCOL_VERSION);
+    UPK_INIT_HELPER(v0_ack_repl_t, REPL_ACK);
+    UPK_END_HELPER(PKT_REPLY);
 }
 
 /* *******************************************************************************************************************
@@ -315,20 +278,13 @@ v0_create_ack_repl(void)
 upk_packet_t           *
 v0_create_error_repl(char *svc_id, char *msg, upk_errlevel_t errlevel)
 {
-    v0_error_repl_t        *payload = NULL;
-    uint32_t                payload_len = 0;
+    UPK_INIT_HELPER(v0_error_repl_t, REPL_ERROR);
 
-    payload = calloc(1, sizeof(*payload));
+    UPK_HELPER_ENUM(errlevel);
+    UPK_HELPER_STRING(msg, msg_len);
+    UPK_HELPER_STRING(svc_id, svc_id_len);
 
-    payload->msgtype = REPL_ERROR;
-    payload->errlevel = errlevel;
-    COPY_STRING_MEMB(msg, msg_len);
-    COPY_STRING_MEMB(svc_id, svc_id_len);
-
-    payload_len =
-        sizeof(*payload) + CALC_ENUM_LEN(msgtype) + CALC_ENUM_LEN(errlevel) + CALC_PTR_LEN(msg_len) +
-        CALC_PTR_LEN(svc_id_len);
-    return upk_create_pkt(payload, payload_len, PKT_REPLY, PROTOCOL_VERSION);
+    UPK_END_HELPER(PKT_REPLY);
 }
 
 
@@ -338,15 +294,8 @@ v0_create_error_repl(char *svc_id, char *msg, upk_errlevel_t errlevel)
 upk_packet_t           *
 v0_create_pub_pubmsg(void)
 {
-    v0_publication_t       *payload = NULL;
-    uint32_t                payload_len = 0;
-
-    payload = calloc(1, sizeof(*payload));
-
-    payload->msgtype = PUB_PUBLICATION;
-
-    payload_len = sizeof(*payload) + CALC_ENUM_LEN(msgtype);
-    return upk_create_pkt(payload, payload_len, PKT_PUBMSG, PROTOCOL_VERSION);
+    UPK_INIT_HELPER(v0_publication_t, PUB_PUBLICATION);
+    UPK_END_HELPER(PKT_PUBMSG);
 }
 
 /* *******************************************************************************************************************
@@ -354,13 +303,6 @@ v0_create_pub_pubmsg(void)
 upk_packet_t           *
 v0_create_pub_cancel(void)
 {
-    v0_cancelation_t       *payload = NULL;
-    uint32_t                payload_len = 0;
-
-    payload = calloc(1, sizeof(*payload));
-
-    payload->msgtype = PUB_CANCELATION;
-
-    payload_len = sizeof(*payload) + CALC_ENUM_LEN(msgtype);
-    return upk_create_pkt(payload, payload_len, PKT_PUBMSG, PROTOCOL_VERSION);
+    UPK_INIT_HELPER(v0_cancelation_t, PUB_CANCELATION);
+    UPK_END_HELPER(PKT_PUBMSG);
 }
