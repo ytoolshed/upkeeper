d14066b80acf39cf9a698742a4ef8f6a904d413e
Mike Schilli <mschilli@yahoo-inc.com>
2011-12-08 15:25:33 -0800

    bogus tests now have xml ending

--- adm/y/build.sh
+++ adm/y/build.sh
@@ -2,4 +2,4 @@
 LD_RUN_PATH="/home/y/lib64" LDFLAGS=-L/home/y/lib64 CPPFLAGS=-I/home/y/include  ./configure --prefix=/home/y --enable-rpath=/home/y/lib64
 make
 mkdir test-results
-/home/y/bin/prove --timer --formatter TAP::Formatter::JUnit adm/y/ptest >test-results/bogus.unit
+/home/y/bin/prove --timer --formatter TAP::Formatter::JUnit adm/y/ptest >test-results/bogus.xml
