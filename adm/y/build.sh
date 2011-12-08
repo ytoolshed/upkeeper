8e22d489d9fbb93477336d57810da886bcb6fc14
Mike Schilli <mschilli@yahoo-inc.com>
2011-12-08 15:36:48 -0800

    only mkdir test-results if it doesn't exist

--- adm/y/build.sh
+++ adm/y/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 LD_RUN_PATH="/home/y/lib64" LDFLAGS=-L/home/y/lib64 CPPFLAGS=-I/home/y/include  ./configure --prefix=/home/y --enable-rpath=/home/y/lib64
 make
-mkdir test-results
+if [ ! -d test-results ]
+then
+    mkdir test-results
+fi
 /home/y/bin/prove --timer --formatter TAP::Formatter::JUnit adm/y/ptest >test-results/bogus.xml
