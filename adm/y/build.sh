031fb65ae2e28d7c8701dcdacdb6512746f4c84d
Mike Schilli <mschilli@yahoo-inc.com>
2011-12-08 15:02:11 -0800

    build script update

---
+++ adm/y/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+LD_RUN_PATH="/home/y/lib64" LDFLAGS=-L/home/y/lib64 CPPFLAGS=-I/home/y/include  ./configure --prefix=/home/y --enable-rpath=/home/y/lib64
+make
+/home/y/bin/prove --timer --formatter TAP::Formatter::JUnit adm/y/ptest
