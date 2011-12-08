cef6ef281afd59f277ec947ad5653477a2b7fc08
Mike Schilli <mschilli@yahoo-inc.com>
2011-12-08 15:19:56 -0800

    added test-results for hudson build

--- adm/y/build.sh
+++ adm/y/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 LD_RUN_PATH="/home/y/lib64" LDFLAGS=-L/home/y/lib64 CPPFLAGS=-I/home/y/include  ./configure --prefix=/home/y --enable-rpath=/home/y/lib64
 make
-/home/y/bin/prove --timer --formatter TAP::Formatter::JUnit adm/y/ptest
+mkdir test-results
+/home/y/bin/prove --timer --formatter TAP::Formatter::JUnit adm/y/ptest >test-results/bogus.unit
