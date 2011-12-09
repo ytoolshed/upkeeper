ffc9ecf28371c35af3173e7459bbf306385d3de9
Mike Schilli <mschilli@yahoo-inc.com>
2011-12-08 17:58:37 -0800

    make clean

--- adm/y/build.sh
+++ adm/y/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if [ -f Makefile ]
+then
+    make clean
+fi
 LD_RUN_PATH="/home/y/lib64" LDFLAGS=-L/home/y/lib64 CPPFLAGS=-I/home/y/include  ./configure --prefix=/home/y --enable-rpath=/home/y/lib64
 make
 rm -rf test-results
