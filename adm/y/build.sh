a4e655ec8f5aae6ad911e9b5aea7fa9e1dac64d4
Mike Schilli <mschilli@yahoo-inc.com>
2011-12-08 17:02:14 -0800

    test-results now deleted between builds

--- adm/y/build.sh
+++ adm/y/build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 LD_RUN_PATH="/home/y/lib64" LDFLAGS=-L/home/y/lib64 CPPFLAGS=-I/home/y/include  ./configure --prefix=/home/y --enable-rpath=/home/y/lib64
 make
-if [ ! -d test-results ]
-then
-    mkdir test-results
-fi
+rm -rf test-results
+mkdir test-results
 /home/y/bin/prove --timer --formatter TAP::Formatter::JUnit adm/y/ptest >test-results/bogus.xml
